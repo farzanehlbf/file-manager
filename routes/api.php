1a5566049b8eef0cd10d1c18a0c12389e94ebce5
farzaneh labafi <85051697+farzanehlbf@users.noreply.github.com>
2025-06-01 11:21:32 +0330

    feat: add Auth

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\FileController;
 use App\Http\Controllers\Api\FolderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
 
